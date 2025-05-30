ac20c1e5b12394f51236e35b5708983609d884ef
qc <qiuchen610714354@gmail.com>
2025-05-30 18:17:48 +0800

    feat: 文字属性的部分修改

--- fditor-ui/src/views/editer/components/propertyBar/textBar/text-bar.vue
+++ fditor-ui/src/views/editer/components/propertyBar/textBar/text-bar.vue
@@ -8,27 +8,34 @@
   import { createLinearGradient, createRadialGradient, type colorVal, type Editor } from '@kditor/core'
   import { colorInstance2Info } from '@/utils/common'
   import { useGetAttrs } from '@/hooks/useGetAttrs'
+  import { Textbox } from 'fabric'
   const editorStore = useEditorStore()
   const editor = inject(EditorKey) as Editor
   const selected = editorStore.selected
+  const fontFamilyList = ['abc', 'def', 'hik', 'era']
 
   interface IFontAttrs {
     fill: ColorInfo
-    stroke: ColorInfo
-    strokeWidth: number
-    dash: number[]
-    width: number
+    fontfamily: string
+    fontsize: number
   }
   const attrs: Partial<IFontAttrs> = reactive({
     fill: {
       type: 'solid',
       value: 'rgba(255,255,255,1)'
-    }
+    },
+    fontfamily: '',
+    fontsize: 10
   })
 
   function getAttrs() {
+    console.log('get font attr')
     if (!selected) throw new Error('get font color but no selected')
+    if (!(selected instanceof Textbox)) throw new Error('get attr but is not textbox')
     attrs.fill = colorInstance2Info(selected.fill as colorVal)
+    attrs.fontfamily = 'abc'
+    console.error(selected.fontSize)
+    attrs.fontsize = selected.fontSize
   }
   useGetAttrs(getAttrs)
 
@@ -73,12 +80,20 @@
     }
     editor.render()
   }
+  function updateFontsize(val: number | undefined) {
+    if (val === undefined) throw Error('update fontsize but value === undefined')
+    if (!selected) throw new Error('update fontsize but no selected')
+    selected.eset('fontSize', val, false)
+    editor.render()
+  }
 </script>
 
 <template>
   <div class="typeBar">
-    <div>字体</div>
-    <div>字号</div>
+    <el-select :model-value="attrs.fontfamily" placeholder="" size="default" style="width: 240px">
+      <el-option v-for="item in fontFamilyList" :key="item" :label="item" :value="item" />
+    </el-select>
+    <el-input-number :model-value="attrs.fontsize" :min="10" @change="updateFontsize" />
     <fill-property :color="attrs.fill" tip="font color" @update:color="updateFill"></fill-property>
     <div>B</div>
     <div>I</div>
