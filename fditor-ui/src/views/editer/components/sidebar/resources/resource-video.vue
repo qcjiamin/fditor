956d177fa9f6502d847a26495651913fd52ef191
qc <qiuchen610714354@gmail.com>
2025-12-17 17:26:23 +0800

    feat: 调整视频页

--- fditor-ui/src/views/editer/components/sidebar/resources/resource-video.vue
+++ fditor-ui/src/views/editer/components/sidebar/resources/resource-video.vue
@@ -1,25 +1,13 @@
 <script lang="ts" setup>
-  // import { ref } from 'vue'
-  import colorPicker from '@/components/colorPicker/color-picker.vue'
-  import { onMounted, ref, watch } from 'vue'
-  const colorRef = ref('rgba(255, 255, 255, 1)')
-  // function updateColor(color: string) {
-  //   console.log('updateColor', color)
-  //   colorRef.value = color
-  // }
-  watch(colorRef, (color) => {
-    console.log('updateColor', color)
-    // const div = document.querySelector('.resouceVideoeBox') as HTMLDivElement
-    // div.style.backgroundColor = colorRef.value
-  })
-  onMounted(() => {
-    // 获取颜色时，默认将其类型转换为rgb来处理
-  })
+  import { onMounted } from 'vue'
+  import resourceHeader from '@/views/editer/components/sidebar/resources/components/resource-header.vue'
+
+  onMounted(() => {})
 </script>
 
 <template>
   <div class="resouceVideoeBox">
-    <color-picker v-model:color="colorRef"></color-picker>
+    <resource-header class="header" title="Image"></resource-header>
   </div>
 </template>
 
